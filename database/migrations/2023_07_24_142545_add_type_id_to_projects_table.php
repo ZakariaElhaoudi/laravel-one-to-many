1846e84adf4eab170a5ed9e9024c85e9839d7c3c
Zakaria96 <zakarialavoro@gmail.com>
2023-07-24 16:58:03 +0200

    ticket

--- database/migrations/2023_07_24_142545_add_type_id_to_projects_table.php
+++ database/migrations/2023_07_24_142545_add_type_id_to_projects_table.php
@@ -14,7 +14,10 @@ return new class extends Migration
     public function up()
     {
         Schema::table('projects', function (Blueprint $table) {
-            //
+            $table->unsignedBigInteger('type_id');
+            $table -> foreign('type_id')
+            -> references('id')
+            -> on('types');
         });
     }
 
@@ -26,7 +29,8 @@ return new class extends Migration
     public function down()
     {
         Schema::table('projects', function (Blueprint $table) {
-            //
+            $table->dropForeign(['type_id']);
+            $table->dropColumn('type_id');
         });
     }
 };
